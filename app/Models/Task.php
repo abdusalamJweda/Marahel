b7b007fa354293c92a96e3ee6fb7bde29b98f1eb
JwedaAbdusalam <jwedaabdusalam@gmail.com>
2022-05-16 10:40:03 +0200

    added tasks fillables

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,6 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'todo',
+        'due_date',
+        'removed',
+        'status',
+        'created_at',
+        'updated_at',
+        'user_id',
+    ];
+    
     public function subTasks(){
         return $this->hasMany('App\Models\SubTask');
     }
