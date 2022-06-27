1234f4e1186325a3e93718fde9d9f96bd36f3b1e
Your Name <you@example.com>
2022-06-27 18:17:42 +0200

    i really dont know but uffa hasn't got the latest version so.more ALMONDS

--- database/factories/PhaseFactory.php
+++ database/factories/PhaseFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 use App\Models\Project;
+use App\Models\Phase;
 
 
 class PhaseFactory extends Factory
@@ -21,8 +22,8 @@ class PhaseFactory extends Factory
             'description' => $this->faker->name(), //$this->faker->unique()->safeEmail(),
             'due_date' => $this->faker->dateTime($max = 'now', $timezone = null),
            
-            'deleted_at' => $this->faker->dateTime($max = 'now', $timezone = null),
-            'status' => $this->faker->numberBetween($min = 0, $max = 1),
+            // 'deleted_at' => $this->faker->dateTime($max = 'now', $timezone = null),
+            // 'status' => $this->faker->numberBetween($min = 0, $max = 1),
             'created_at' => $this->faker->dateTime($max = 'now', $timezone = null),
             'updated_at' => $this->faker->dateTime($max = 'now', $timezone = null),
             'project_id'=> Project::pluck('id')[$this->faker->numberBetween(1, Project::count()-1)],
